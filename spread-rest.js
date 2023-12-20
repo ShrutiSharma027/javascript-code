651031d2740403be238078568921148769a76179
Shruti WebGit <27shrutisharma.06@gmail.com>
2023-12-20 17:55:05 +0530

    JS code uploaded successfully

---
+++ spread-rest.js
@@ -0,0 +1,66 @@
+// rest
+// function age(age1,age2,age3,...other_ages){
+//     console.log(age1,age2,age3)
+//     console.log(other_ages)
+// }
+// age(20,30,40,50,60)
+
+// spread
+// let arr=[10,20,30]
+// let arr1=[...arr,40,50]
+// console.log(arr1)
+// console.log(arr)
+
+// let laptop1=["Dell",50000,2]
+// let laptop2=["Lenovo",65000,3]
+// console.log(laptop1)
+// console.log(laptop2)
+// console.log(...laptop1,laptop2)
+
+// let laptop1={"Name":"Dell",
+//               "Price": 35000,
+//               "Warranty":"1year"
+// }
+// let warranty={"Origin":"India",
+// "Chargerbattery":40000,
+// "Battery_backup":"2years"
+// }
+// let laptop3={...laptop1,...warranty}
+// console.log(laptop3)
+
+const basic={
+    "Firstname":"Shruti"
+}
+const company={
+    "Companyname":"XYZ Lmt."
+}
+const Qualification={
+    "Std":"Graduate"
+}
+const PI={
+    "Age":30
+}
+// console.log(basic.Firstname)
+// console.log(company.Companyname)
+// console.log(Qualification.Std)
+// console.log(PI.Age)
+const profile={...basic,...company,...Qualification,...PI}
+// console.log(profile.Firstname)
+
+// let arr=[1,2,3]
+// let arr1=[...arr]
+// arr1.push(4)
+// console.log(arr)
+// console.log(arr1)
+
+// const array=[10,20,30]
+// const array1=[10,20,30]
+// console.log(array)
+// console.log(array1)
+// const join=[...array,...array1]
+// console.log(join)
+
+
+const str=['my','name','is','kwl']
+console.log(...str)
+
---
+++ variable-var-let-const.js
@@ -0,0 +1,46 @@
+// let a=10;
+// let a=20;
+// console.log(a)
+
+// let b=10;
+//  b=20;
+// console.log(b)
+
+// const c=10;
+//  c=20;
+// console.log(c)
+
+// const g=10;
+// function get(){
+//     const g=20;
+//     console.log(g,"Function")
+//     {
+//         const g=30;   
+//         console.log(g,"Block")
+//     }
+//     console.log(g,"Outside block")
+// }
+// console.log(g,"Global variable")
+// get()
+
+// let a="Hello!"
+// let b="My"
+// let name1="Shruti"
+// console.log(a+ " "+ b+ " name is " +name1)
+
+let name="Shruti"
+console.log(`My name is ${name}`)
+
+//Template Literals
+let myname="Shruti"
+let std="graduation"
+let look="job"
+console.log(`Hello! My name is ${myname}, I have completed my ${std}, and looking for ${look}.`)
+
+let arr=[500,700,900,1200,200]
+const minValue = Math.min(...arr);
+const maxValue = Math.max(...arr);
+
+console.log(`Minimum value is ${minValue} and maximum value is ${maxValue}`);
+// console.log(arr)
+
